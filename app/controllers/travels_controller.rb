624a8d7fea61f813c7f23f01a988fce8006d2e57
ksmshkt <ksmshkt@example.com>
2023-03-10 20:09:39 +0900

    Add CRUD functions for travel model

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -12,14 +12,46 @@ class TravelsController < ApplicationController
   end
 
   def create
+		@travel = Travel.new(travel_params)
+
+    if @travel.save
+      flash[:success] = '新しい旅行が追加されました'
+      redirect_to @travel
+    else
+      flash.now[:danger] = '新しい旅行の追加に失敗しました'
+      render :new
+    end
+
   end
 
   def edit
+    @travel = Travel.find(params[:id])
   end
 
   def update
+    @travel = Travel.find(params[:id])
+
+    if @travel.update(travel_params)
+      flash[:success] = 'travel は正常に更新されました'
+      redirect_to @travel
+    else
+      flash.now[:danger] = 'travel は更新されませんでした'
+      render :edit
+    end
   end
 
   def destroy
+    @travel = Travel.find(params[:id])
+    @travel.destroy
+
+    flash[:success] = '旅行は正常に削除されました'
+    redirect_to travels_url
+  end
+
+	private
+
+	# Strong Parameter
+  def travel_params
+    params.require(:travel).permit(:name)
   end
 end
