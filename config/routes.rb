0e32b46998eb17f41340e5f6af391e112e170b85
ksmshkt <ksmshkt@example.com>
2023-03-14 09:01:29 +0900

    Add user model and signup feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
   resources :travels
+
+  resources :users, only: [:new, :create]
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_05_231541) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_12_033059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "travels", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
