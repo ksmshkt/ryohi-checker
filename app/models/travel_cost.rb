ded05d504678f154ecf73498f8030198f36fc3ee
ksmshkt <ksmshkt@example.com>
2023-03-18 21:55:00 +0900

    Add validation for travel_cost model

--- app/models/travel_cost.rb
+++ app/models/travel_cost.rb
@@ -1,3 +1,21 @@
 class TravelCost < ApplicationRecord
   belongs_to :travel
+
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :budget,
+            presence: true,
+            length: {
+              maximum: 20,
+            },
+            numericality: {
+              only_integer: true,
+            }
+  validates :amount_paid,
+            presence: true,
+            length: {
+              maximum: 20,
+            },
+            numericality: {
+              only_integer: true,
+            }
 end
